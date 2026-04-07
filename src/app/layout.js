ebd43958d8492ba666e7cc1e0504d99de7d98b16
Troygit config --global user.email <tydamon@hotmail.com>
2026-04-07 23:06:15 +1000

    added authcontext

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "../../lib/authContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +19,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">
+
+        <AuthProvider>
+
+          {children}
+
+        </AuthProvider>
+
+      </body>
     </html>
   );
 }
